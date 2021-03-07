00264848e2241d760e7e83f041113028f75e6065
Ronny Mandal <ronnyma@transfinite.io>
2021-03-07 13:28:43 +0100

    More fix

--- main.py
+++ main.py
@@ -6,37 +6,39 @@ import tweepy as tw
 
 import persistence as p
 
-consumer_key= 'gCaaWlM7VvKNJOjkZbubTMTST'
-consumer_secret= 'nB4syMYO7WpyXcFkXjfgbT9ePHPm5O3E1Go5RHO8X1paiTJ4P8'
-access_token= '4287816021-jQxCWgqXpl3PuueFFMpTqEQ3fUt2oUSQwmEqBRz'
-access_token_secret= 'wblNZe49Rwh6v78TgqLv0Il5V1bTdJq0Zj74JUt0BD2UV'
+consumer_key = 'gCaaWlM7VvKNJOjkZbubTMTST'
+consumer_secret = 'nB4syMYO7WpyXcFkXjfgbT9ePHPm5O3E1Go5RHO8X1paiTJ4P8'
+access_token = '4287816021-jQxCWgqXpl3PuueFFMpTqEQ3fUt2oUSQwmEqBRz'
+access_token_secret = 'wblNZe49Rwh6v78TgqLv0Il5V1bTdJq0Zj74JUt0BD2UV'
+
 
 def init():
     auth = tw.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tw.API(auth, wait_on_rate_limit=True)
+
     return api
+
 
 def get_tweet(api):
     search_words = "#dax18"
     date_since = date.today()
+    db = p.init()
+    it = 0
+
     tweets = tw.Cursor(api.search,
                        tweet_mode="extended",
                        q=search_words,
                        lang="no",
                        since=date_since).items(5000)
-    db = p.init()
-    it = 0
+
     for t in tweets:
         p.insert(db, {'tweet': t._json, 'fulltext': t.full_text})
         it = it + 1
 
-    f = open("status", "w+")
-    f.write(f'Fetched {it} tweets.')
-    f.close()
+    print(f'{date_since}: Fetched {it} tweets.')
+    db.close()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     get_tweet(init())
-
